03ced23e9e00128ff3f7b3cd2fbc457ec859dda3
charinafaye7 <52297715+charinafaye7@users.noreply.github.com>
2025-03-28 08:03:48 -0700

    Update 2. Amount_of_Addresses.sql

--- 2. Amount_of_Addresses.sql
+++ 2. Amount_of_Addresses.sql
@@ -2,9 +2,7 @@
 		Using the address table, return how many addresses are in each district.
 */
 
-SELECT 
-    district, COUNT(*) as 'addresses'
-FROM
-    address
+SELECT district, COUNT(*) as 'addresses'
+FROM address
 GROUP BY district
 ORDER BY district;
