0f276083835de33f7712e971996cbd20212fb50a
charinafaye7 <52297715+charinafaye7@users.noreply.github.com>
2025-03-28 08:08:01 -0700

    Add files via upload

---
+++ 3. Find_Specific_Films.sql
@@ -0,0 +1,8 @@
+/* Exercise 3:
+		Select the title, rental_rate, and replacement_cost from the film table. Write a query that finds the 
+        films that have a rental_rate less than a dollar or a replacement_cost les than 15 dollars.
+*/
+
+SELECT title, rental_rate, replacement_cost
+FROM film
+WHERE rental_rate < 1 OR replacement_cost < 15;
